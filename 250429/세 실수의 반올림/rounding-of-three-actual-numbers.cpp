b51f038c8efff9db9f8896813ee65bd748decf8f
sheepdain <didek5123@naver.com>
2025-04-29 23:33:35 +0900

    Title: 세 실수의 반올림, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    double b;
+    double c;
+    cin >> a >> b >> c ;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+    // Please write your code here.
+    return 0;
+}
