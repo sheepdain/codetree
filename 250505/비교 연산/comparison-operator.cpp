72ac3a41c9e9dc980f0c7eb662d9c9460af0185b
sheepdain <didek5123@naver.com>
2025-05-05 14:52:19 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250505/비교 연산/comparison-operator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    if (A>=B){
+        cout << 1 << "\n";
+    }
+    else {
+        cout << 0<< "\n";
+    }
+    if (A>B){
+        cout << 1<< "\n";
+    }
+    else {
+        cout << 0<< "\n";
+    }
+    if (B>=A){
+        cout << 1<< "\n";
+    }
+    else {
+        cout << 0<< "\n";
+    }
+    if (B>A){
+        cout << 1<< "\n";
+    }
+    else {
+        cout << 0<< "\n";
+    }
+    if (B==A){
+        cout << 1<< "\n";
+    }
+    else {
+        cout << 0<< "\n";
+    }if (B!=A){
+        cout << 1<< "\n";
+    }
+    else {
+        cout << 0<< "\n";
+    }
+    return 0;
+}
