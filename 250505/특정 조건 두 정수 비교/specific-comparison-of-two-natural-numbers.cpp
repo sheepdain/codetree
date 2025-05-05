5389c7e47d054802ed54d6d1a86f80079b80a52d
sheepdain <didek5123@naver.com>
2025-05-05 15:01:53 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250505/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    if (A<B){
+        cout <<1 <<" ";
+    }
+    else{
+        cout <<0<<" ";
+    }
+    if(A==B){
+        cout << 1;
+    }
+    else{
+        cout <<0;
+    }
+    return 0;
+}
