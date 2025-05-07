24f0b3feaff241285cc021db83adcab54242971d
sheepdain <didek5123@naver.com>
2025-05-07 23:25:46 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char A;
+    cin >> A;
+    int i;
+    for (i=0;i<8;i++){
+        cout <<A;
+    }
+    return 0;
+}
