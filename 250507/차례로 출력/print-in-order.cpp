f5e690acb2a5bf36d74dc4e32c6093e72f3d3d99
sheepdain <didek5123@naver.com>
2025-05-07 23:30:47 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/차례로 출력/print-in-order.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i;
+    for (i=5;i<18;i++){
+        cout << i << " ";
+    }
+    return 0;
+}
